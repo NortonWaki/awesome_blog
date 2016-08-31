5be4cd54d84ad6bf240a774d7cbdd71cda9714e5
NortonWaki <nortonnw@hotmail.com>
2016-08-30 22:34:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,23 @@ class MainHandler(webapp2.RequestHandler):
   def get(self):
     self.response.out.write(
       """
-        Teste
+      <html>
+        <head>
+          <title>Awesome blog</title>
+        </head>
+        <body>
+          <form method="post">
+            <input type="texte" name="nome">
+            <input type="submit">
+          </form>
+        </body>
+      </html>
       """
     )
-
+    
+  def post(self):
+    texto = self.request.get("nome")
+    self.response.out.white(texto)
 
 app = webapp2.WSGIApplication([
   ('/', MainHandler)
